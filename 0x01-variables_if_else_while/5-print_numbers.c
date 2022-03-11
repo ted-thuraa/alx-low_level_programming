bebd086eb61b76b173298699f6534066b2959e86
ted-thuraa <mutwirited@gmail.com>
2022-03-11 07:38:31 -0800

    add

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - prints out 0-9 and a newline
+ *
+ * Description: Program that prints out numbers
+ *
+ * Return: int 0 when done and no errors
+ */
+
+
+
+int main(void)
+
+{
+
+	puts("0123456789");
+
+	return (0);
+
+}
+
+
