e45651dbf1e4cbf707069d78bdebbef558fc68bc
ted-thuraa <mutwirited@gmail.com>
2022-04-11 05:30:04 -0700

    macros

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - prints the name of the file it was compiled from
+ *
+ * Return: NULL
+ */
+int main (void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+0-object_like_macro.h -  a header file that defines a macro named SIZE as an abbreviation for the token 1024<br />
